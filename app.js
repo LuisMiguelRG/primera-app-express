78997c1596df182105d02af3707a37886f854420
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-03-23 19:21:36 -0500

    codigo secreto v.2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<div class="secret" style="display: none;">HcUy6Re2LLBRtj</div>');
+  res.send('<div class="secret" style="display: none;">HcUy6Re2LLBRtj</div><h1> Hola Mundo</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
