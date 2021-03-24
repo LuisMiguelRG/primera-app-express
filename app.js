668d68860f26e959fe8575585d4025f66601c8a3
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-03-23 20:34:26 -0500

    saludame 1 v.6

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('?nombre=Juan', (req, res) => {
-  res.send(validacion)  
+  res.send(req.query.name ? "<h1>Hola " + req.query.name + "!</h1>" : "<h1>Hola desconocido!</h1>)  
 });
-var validacion = req.query.name; 
-  if (validacion == " ") {
-    "<h1> Hola desconocido!</h1>"
-  }else{
-    "<h1> Hola " + validacion +"!</h1>"
-  }
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
