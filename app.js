33b7a4f3b80d25d6c807775a3f6c4f9694d49261
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-03-23 19:54:54 -0500

    saludame 1 v.1

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ app.get('/', (req, res) => {
   res.send(validacion)  
 });
 var validacion = req.query.name; 
-  if (req.query.name == " ") {
+  if (validacion == " ") {
     "<h1> Hola Desconocido</h1>"
   }else{
-    "<h1> Hola </h1>" + req.query.name
+    "<h1> Hola </h1>" + validacion
   }
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
