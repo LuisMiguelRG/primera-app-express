40297741862aa2795d84b747af2e8059b682cc9d
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-03-25 20:45:23 -0500

    saludame 2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/makers/juan', (req, res) => {
   var validacion = req.params.nombre; 
   validacion = validacion.replace(/^\w/, c => c.toUpperCase());
-    res.send("<h1>Hola " + validacion +"!</h1>");
+    console.log(res.send("<h1>Hola " + validacion +"!</h1>"));
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
